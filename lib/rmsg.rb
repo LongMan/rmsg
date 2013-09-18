423e622a2ccf21cb5cf241f439d94068b97b685e
Alex Gusev <alx.gsv@gmail.com>
2013-09-18 18:00:47 +0400

    comments and durability spec

--- lib/rmsg.rb
+++ lib/rmsg.rb
@@ -1,35 +1,75 @@
 require "yajl"
 
+# Redis messaging
+#
+# Internal message format stored in redis is json encoded hash:
+#
+# { m: message, r: { l: response_list, e: response_expires_in } }
+#
+# message             - anything encodable to json
+# response_list       - on which redis list client awaits for response
+# response_expires_in - ttl in seconds for response_list
 module Rmsg
+
+  class Rmsg::TimeoutError < StandardError
+  end
+
   module_function
-  def send(queue, options={})
-    redis = options.delete(:redis) || Redis.current
+
+  # Module function: Send message to queue.
+  #
+  # queue - queue name
+  # message - something jsonable to send to queue
+  # options - The Hash options used to refine the selection (default: {}):
+  #           :redis              - redis client to use instead of Redis.current
+  #           :timeout            - timeout in seconds when stop to wait for answer
+  #           :response_expire_in - after which period of time expire response if client crash waiting for it
+  #
+  # Returns server response string.
+  def send(queue, message, options={})
+    redis = options[:redis] || Redis.current
+    timeout = options[:timeout]
     response_list = SecureRandom.uuid
-    puts "sending #{options.inspect} to #{queue} via #{redis.inspect}"
-    redis.rpush(queue, Yajl.dump(options.merge(respond_to: response_list)))
-    q, response = redis.blpop(response_list)
+    response_expire_in = options[:response_expire_in]
+
+    redis.rpush(queue, Yajl.dump({m: message, r: { l: response_list, e: response_expire_in}}))
+    q, response = redis.blpop(response_list, timeout: timeout)
+    raise Rmsg::TimeoutError if q.nil? && response.nil?
     redis.del(response_list)
     response
   end
 
+  # Module function: Process messages and optionally send responses.
+  #
+  # queue - array of string of queue names or one queue name
+  # options - The Hash options used to refine the selection (default: {}):
+  #           :redis              - redis client to use instead of Redis.current
+  #
+  # Yields the String queue name and Hash client message.
+  #
+  # Returns nothing.
   def serve(queues, options={})
-    puts "server starting"
     redis = options.delete(:redis) || Redis.current
 
     raise StandardError if !queues
 
     queues = [queues] if !queues.is_a?(Array)
-    puts "serving #{queues.inspect} with #{redis.inspect}"
 
     while true
-      puts "waiting for message"
-      queue, message = redis.blpop queues
-      message = Yajl.load(message)
-      puts "got #{message}"
-      response = yield(queue, message)
-      puts "response is #{response}"
-      redis.rpush(message["respond_to"], response)
-      puts "responded to #{message["respond_to"]}"
+      queue, payload = redis.blpop queues
+      payload = Yajl.load(payload)
+      message = payload["m"]
+      respond_to = (payload["r"] || {})["l"]
+      response_expire_time = (payload["r"] || {})["e"] || 60*10
+      response = begin
+        yield(queue, message)
+      rescue => e
+        Yajl.dump({error: e.to_s})
+      end
+      if respond_to
+        redis.rpush(respond_to, response)
+        redis.expire(respond_to, response_expire_time)
+      end
     end
   end
 end
--- test/test_messaging.rb
+++ test/test_messaging.rb
@@ -2,20 +2,89 @@ require "helper"
 require "rmsg"
 
 class RmsgClientTest < Minitest::Test
+  def setup
+    @client_redis = Redis.new
+    @server_redis = Redis.new
+    @queue = SecureRandom.uuid
+  end
+
+  def teardown
+    Thread.kill(@server)
+  end
+
   def test_simple_response
-    client_redis = Redis.new
-    queue = SecureRandom.uuid
-    server = Thread.new do
-      server_redis = Redis.new
-      Rmsg.serve(queue, redis: server_redis) do |q, msg|
+    @server = Thread.new do
+      Rmsg.serve(@queue, redis: @server_redis) do |q, msg|
         "42"
       end
     end
 
     10.times do
-      assert_equal Rmsg.send(queue, {redis: client_redis}), "42"
+      assert_equal Rmsg.send(@queue, {}, {redis: @client_redis}), "42"
+    end
+  end
+
+  def test_timeout
+    @server = Thread.new do
+      Rmsg.serve(@queue, redis: @server_redis) do |q, msg|
+        sleep 10
+        "42"
+      end
     end
 
-    Thread.kill(server)
+    assert_raises Rmsg::TimeoutError do
+      Rmsg.send(@queue, {}, {redis: @client_redis, timeout: 1})
+    end
   end
+
+  def test_unused_response_channels
+    @server = Thread.new do
+      Rmsg.serve(@queue, redis: @server_redis) do |q, msg|
+        sleep 2
+        "42"
+      end
+    end
+
+    begin
+      Rmsg.send(@queue, {}, {redis: @client_redis, timeout: 1, response_expire_in: 2})
+    rescue Rmsg::TimeoutError
+    end
+    sleep 2
+    keys_count_1 = @client_redis.info("keyspace")["db0"].split(",").first.split("=").last.to_i
+    sleep 1
+    keys_count_2 = @client_redis.info("keyspace")["db0"].split(",").first.split("=").last.to_i
+    assert_equal keys_count_1 - 1, keys_count_2
+  end
+
+  def test_server_durability
+    i = 0
+    @server = Thread.new do
+      Rmsg.serve(@queue, redis: @server_redis) do |q, msg|
+        i += 1
+        (1/(i - 1)).to_s
+      end
+    end
+
+    r = Rmsg.send(@queue, {}, {redis: @client_redis})
+    r = Rmsg.send(@queue, {}, {redis: @client_redis})
+    assert_equal r, "1"
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
