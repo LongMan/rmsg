0476d6aacc52862f83f083170643c697eca8b7f8
Alex Gusev <alx.gsv@gmail.com>
2013-09-19 11:27:52 +0400

    New message format with header

--- lib/rmsg.rb
+++ lib/rmsg.rb
@@ -2,16 +2,18 @@ require "yajl"
 
 # Redis messaging
 #
-# Internal message format stored in redis is json encoded hash:
+# Internal message format is json encoded hash:
 #
-# { m: message, r: { l: response_list, e: response_expires_in } }
+# { h: header, b: body }
 #
-# message             - anything encodable to json
-# response_list       - on which redis list client awaits for response
-# response_expires_in - ttl in seconds for response_list
+# body                - required, anything encodable to json
+# header              - optional, hash with message metadata
 module Rmsg
 
   class Rmsg::TimeoutError < StandardError
+  end
+
+  class Rmsg::RequestProcessingError < StandardError
   end
 
   module_function
@@ -20,7 +22,7 @@ module Rmsg
   #
   # queue - queue name
   # message - something jsonable to send to queue
-  # options - The Hash options used to refine the selection (default: {}):
+  # options - The Hash options (default: {}):
   #           :redis              - redis client to use instead of Redis.current
   #           :timeout            - timeout in seconds when stop to wait for answer
   #           :response_expire_in - after which period of time expire response if client crash waiting for it
@@ -32,11 +34,15 @@ module Rmsg
     response_list = SecureRandom.uuid
     response_expire_in = options[:response_expire_in]
 
-    redis.rpush(queue, Yajl.dump({m: message, r: { l: response_list, e: response_expire_in}}))
+    redis.rpush(queue, Yajl.dump({b: message, h: { l: response_list, e: response_expire_in}}))
     q, response = redis.blpop(response_list, timeout: timeout)
     raise Rmsg::TimeoutError if q.nil? && response.nil?
     redis.del(response_list)
-    response
+    response = Yajl.load(response)
+    if response["h"] && response["h"]["error"]
+      raise Rmsg::RequestProcessingError, response["h"]["error"]
+    end
+    response["b"]
   end
 
   # Module function: Process messages and optionally send responses.
@@ -56,19 +62,19 @@ module Rmsg
     queues = [queues] if !queues.is_a?(Array)
 
     while true
-      queue, payload = redis.blpop queues
-      payload = Yajl.load(payload)
-      message = payload["m"]
-      respond_to = (payload["r"] || {})["l"]
-      response_expire_time = (payload["r"] || {})["e"] || 60*10
+      queue, msg = redis.blpop queues
+      msg = Yajl.load(msg)
+      respond_to = (msg["h"] || {})["l"]
+      response_expire_time = (msg["h"] || {})["e"] || 60*10
       response = begin
-        yield(queue, message)
+        { b: yield(queue, msg["b"]) }
       rescue => e
-        Yajl.dump({error: e.to_s})
+        # if something went wrong responding with error
+        { h: {error: e.to_s}, b: "" }
       end
       if respond_to
-        redis.rpush(respond_to, response)
-        redis.expire(respond_to, response_expire_time)
+        redis.rpush(respond_to, Yajl.dump(response))
+        redis.pexpire(respond_to, (response_expire_time*1000).to_i)
       end
     end
   end
--- test/test_messaging.rb
+++ test/test_messaging.rb
@@ -38,22 +38,30 @@ class RmsgClientTest < Minitest::Test
   end
 
   def test_unused_response_channels
+    # 0s client sends message with timeout 1s, expire 0.5s
+    # 0s server receives message, starts sleeping
+    # 1s client stops waiting (stops blpop on queue)
+    # 2s server ends sleeping and sends response
+    # 2s response key created, will expire in 0.5s
+    # 2.5s response key expired and no longer exists
+
+    keys_count_before = @client_redis.info("keyspace")["db0"].split(",").first.split("=").last
+
     @server = Thread.new do
       Rmsg.serve(@queue, redis: @server_redis) do |q, msg|
         sleep 2
         "42"
       end
     end
-
+    # 0s
     begin
-      Rmsg.send(@queue, {}, {redis: @client_redis, timeout: 1, response_expire_in: 2})
+      Rmsg.send(@queue, {}, {redis: @client_redis, timeout: 1, response_expire_in: 0.5})
     rescue Rmsg::TimeoutError
     end
-    sleep 2
-    keys_count_1 = @client_redis.info("keyspace")["db0"].split(",").first.split("=").last.to_i
-    sleep 1
-    keys_count_2 = @client_redis.info("keyspace")["db0"].split(",").first.split("=").last.to_i
-    assert_equal keys_count_1 - 1, keys_count_2
+    # 1s
+    sleep 1.5
+    keys_count_after = @client_redis.info("keyspace")["db0"].split(",").first.split("=").last
+    assert_equal keys_count_before, keys_count_after
   end
 
   def test_server_durability
@@ -65,26 +73,10 @@ class RmsgClientTest < Minitest::Test
       end
     end
 
-    r = Rmsg.send(@queue, {}, {redis: @client_redis})
+    assert_raises Rmsg::RequestProcessingError do
+      Rmsg.send(@queue, {}, {redis: @client_redis})
+    end
     r = Rmsg.send(@queue, {}, {redis: @client_redis})
     assert_equal r, "1"
   end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 end
